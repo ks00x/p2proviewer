22590d4581ffea18d1f2828aacc29393c972faae
ks <transients@gmx.de>
2024-01-13 12:26:12 +0100

    x

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 - extract the raw data out of Infiray P2Pro thermal camera jpeg images
 - visualize data
 - save temperature data as csv file
+- temperature in Celsius
 - streamlit app [Streamlit • A faster way to build and share data apps](https://streamlit.io/)
 
 [live version of the app](https://p2proviewer.streamlit.app/)
--- p2profile.py
+++ p2profile.py
@@ -7,14 +7,13 @@ https://www.eevblog.com/forum/thermal-imaging/infiray-and-their-p2-pro-discussio
 
 '''
 
-def p2pro_image(filename):
+def p2pro_image(fileobj):
     'extracts the raw data from a p2pro file and returns the temperature map in Celsius'
-    im = Image.open(filename)
+    im = Image.open(fileobj)
     a = im.applist[2][1]
     for i in range(3, 7):
         a += im.applist[i][1]
-    # https://pillow.readthedocs.io/en/stable/handbook/concepts.html#concept-modes
-    # this setting at least gives an image that looks like the original thermal image. Details unclear..
+    # https://pillow.readthedocs.io/en/stable/handbook/concepts.html#concept-modes    
     img = Image.frombytes('I;16N' , (256, 192), a[0xC000*2:])
     temps = np.array(img,dtype=np.int32) 
     return (temps / 64) - 273.15
@@ -27,3 +26,6 @@ def main():
     plt.imshow(temps, cmap="inferno")
     plt.show()
 
+
+if __name__ == '__main__':
+    main()
